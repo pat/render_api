cf8877f79352d89d9759ba89f69e3418e5286da5
David Mauskop <david@render.com>
2022-02-28 15:53:42 -0800

    Add support for jobs endpoints

---
+++ lib/render_api/clients/jobs.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative "./base"
+
+module RenderAPI
+  module Clients
+    class Jobs < Base
+      def create(service_id, start_command:, plan_id: nil)
+        body = { startCommand: start_command }
+        body[:planId] = plan_id unless plan_id.nil?
+
+        endpoint.post(
+          "/services/#{service_id}/jobs", body: body
+        )
+      end
+
+      def find(service_id, job_id)
+        endpoint.get(
+          "/services/#{service_id}/jobs/#{job_id}"
+        )
+      end
+
+      def list(service_id, ...)
+        endpoint.get(
+          "/services/#{service_id}/jobs", params: list_parameters(...)
+        )
+      end
+    end
+  end
+end
